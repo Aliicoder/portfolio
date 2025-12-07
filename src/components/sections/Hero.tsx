2ee19f8c6bd23847128789e01c47ea8c235b2948
ali_fahmi <k.c.o.c.3000@gmail.com>
2025-12-07 20:02:09 +0530

    reduce the lcp of hero image

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -9,11 +9,12 @@ const Image = () => {
   return (
     <div
       className={cn(
-        "basis-full  flex justify-center items-center",
+        "basis-full flex justify-center items-center",
         "md:basis-1/2"
       )}
     >
       <img
+        fetchPriority="high"
         className="size-[200px] object-cover md:size-auto "
         src={HeroImage}
         alt="hero-image"
